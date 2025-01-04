409d3c4950fb14546dfdb6fbb496763b824d4a40
Donthireddy <donthireddy431@gmail.com>
2025-01-04 17:12:14 +0530

    added arguments

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hello $PERSON2"
+echo "$PERSON2 :: hi $PERSON1"
+echo "$PERSON1 :: How are you?"
+echo "$PERSON2:: I am doing good, How are you?"
+echo "$PERSON1 :: I am fine"
