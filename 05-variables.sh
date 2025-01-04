eefea4109929cf65020a377cf612762336b14958
Donthireddy <donthireddy431@gmail.com>
2025-01-04 17:37:38 +0530

    converation added

---
+++ 05-variables.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Please enter username : "  # text entered here will be added as value to the variable
+read  USERNAME :  # here USERNAME is the variable name 
+echo "username entered :$USERNAME"
