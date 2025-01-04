835e6de7302d2de7ad5360ac99aca47855d9fa82
Donthireddy <donthireddy431@gmail.com>
2025-01-04 22:18:48 +0530

    sum calculation

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -3,5 +3,9 @@ NUMBER1=$1
 NUMBER2=$2
 TIMESTAMP=$(date)
 echo "script is executed :: $TIMESTAMP"
+# addition
 SUM=$(($NUMBER1+$NUMBER2))
 echo "the sum of $NUMBER1 and $NUMBER2 is : $SUM"
+# multiplication
+MUL=(($NUMBER1*$NUMBER2))
+echo " the multiplication of $NUMBER1 and $NUMBER2 is : $MUL
