0c392f48fdb6215325ac6b44c655d62d490d9363
Donthireddy <donthireddy431@gmail.com>
2025-01-04 22:29:08 +0530

    list of movies

---
+++ 08-array-list.sh
@@ -0,0 +1,9 @@
+#!.bin/bash
+movies=["Devara" "Pushpa-2" "Kalki" "Game Changer",143]
+
+echo "First movie is ${movies[0]}"
+echo "Second movie is ${movies[1]}"
+echo "Third movie is ${movies[2]}"
+echo "Fourth movie is ${movies[3]}"
+echo "Fifth movie is ${movies[4]}"
+echo "all movies list ${movies[@]}"
