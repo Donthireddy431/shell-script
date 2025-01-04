6b3c086f5a37fec3168212f74ab3db67953a10d6
Donthireddy <donthireddy431@gmail.com>
2025-01-04 22:47:58 +0530

    list of movies

---
+++ 09-special-vars.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "All variables passed : $@"
+  
+echo "Number of variables: $#"
+  
+echo "script name : $0"
+  
+echo "Present working directory : $PWD"
+  
+echo "Home directory of current user : $HOME"
+  
+echo "Which user is running this script : $USER"
+  
+echo "Process id of current script : $$"
+
+sleep 60 $
+  
+echo "Process id of last command in background : $!"
