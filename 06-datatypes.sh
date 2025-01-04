941fad4736e5ce97b70062932886d841fdb9c780
Donthireddy <donthireddy431@gmail.com>
2025-01-04 22:16:48 +0530

    sum calculation

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 NUMBER1=$1
 NUMBER2=$2
-SUM=(($NUMBER1+$NUMBER2))
-echo "the sum of $NUMBER1 and $NUMBER2 is :: $SUM"
-echo "script is executed at $date"
+TIMESTAMP=$(date)
+echo "script is executed :: $TIMESTAMP"
+SUM=$(($NUMBER1+$NUMBER2))
+echo "the sum of $NUMBER1 and $NUMBER2 is : $SUM"
